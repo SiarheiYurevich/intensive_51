61b357025a77791902e8673864ec98782ef4d51d
Ivan_Prikhodko <ivanprh2004@gmail.com>
2024-02-15 20:24:56 +0300

    task_1

--- src/test/java/ivan/prh/ArrayListIvanPrikhodkoTest.java
+++ src/test/java/ivan/prh/ArrayListIvanPrikhodkoTest.java
@@ -1,5 +1,5 @@
 package ivan.prh;
-import static org.assertj.core.api.Assertions.assertThat;
+import  static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 
 import java.util.Comparator;
