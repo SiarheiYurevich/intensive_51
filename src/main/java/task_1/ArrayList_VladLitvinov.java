22b8bf6705b23f73c938ffa15dfb75dde6f004b4
LitvinovVlad <itvinovvlad048@gmail.com>
2024-02-17 22:13:00 +0300

    init

--- src/main/java/task_1/ArrayList_VladLitvinov.java
+++ src/main/java/task_1/ArrayList_VladLitvinov.java
@@ -1,6 +1,9 @@
 package task_1;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 public class ArrayList_VladLitvinov<E> implements IntensiveList<E> {
     private E[] array;
@@ -18,7 +21,7 @@ public class ArrayList_VladLitvinov<E> implements IntensiveList<E> {
     @Override
     public int size() {
         int size = array.length;
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < array.length; i++) {
             if (array[i] == null)
                 size -= 1;
         }
@@ -72,31 +75,44 @@ public class ArrayList_VladLitvinov<E> implements IntensiveList<E> {
 
     @Override
     public E remove(int index) {
+        E e = array[index];
         E[] arrayWithNewElement = (E[]) new Object[capacity];
-        E[] arrayNumOne = (E[]) new Object[index + 1];
-        E[] arrayNumTwo;
-
-        arrayNumTwo = (E[]) new Object[arrayWithNewElement.length - arrayNumOne.length];
+        E[] arrayNumOne = (E[]) new Object[index];
+        E[] arrayNumTwo = (E[]) new Object[arrayWithNewElement.length - arrayNumOne.length];
 
         System.arraycopy(array, 0, arrayNumOne, 0, index);
-        arrayNumOne[index] = element;
-        System.arraycopy(array, index, arrayNumTwo, 0, size() - index);
+        System.arraycopy(array, index, arrayNumTwo, 0, size() - index - 1);
 
         System.arraycopy(arrayNumOne, 0, arrayWithNewElement, 0, arrayNumOne.length);
         System.arraycopy(arrayNumTwo, 0, arrayWithNewElement, arrayNumOne.length, arrayNumTwo.length);
 
-        System.arraycopy(arrayWithNewElement, 0, array, 0, size() + 1);
-        return null;
+        System.arraycopy(arrayWithNewElement, 0, array, 0, size() - 1);
+        return e;
     }
 
     @Override
     public void clear() {
-
+        capacity = 10;
+        for (int i = 0; i < size(); i++) {
+            array[i] = null;
+        }
     }
 
     @Override
     public void quickSort(Comparator<E> comparator) {
+        List<E> list = List.of(array);
 
+        Collections.sort(list, comparator);
+    }
+
+    public static void main(String[] args) {
+        ArrayList_VladLitvinov<String> stringArrayListVladLitvinov = new ArrayList_VladLitvinov<>(11);
+        stringArrayListVladLitvinov.add("DDD");
+        stringArrayListVladLitvinov.add("DDED");
+        stringArrayListVladLitvinov.add("DDDD");
+        Comparator<String> stringComparator = (o1, o2) -> o2.length() - o1.length();
+        stringArrayListVladLitvinov.quickSort(stringComparator);
+        System.out.println(stringArrayListVladLitvinov);
     }
 
     @Override
