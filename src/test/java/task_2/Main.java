bfcdbd4893a5c285aca8740aa5a60f95887b305f
Irakli Barikhashvili <wildheader@gmail.com>
2024-02-19 18:45:17 +0300

    fix: remove throws sections from main method

--- src/test/java/task_2/Main.java
+++ src/test/java/task_2/Main.java
@@ -1,9 +1,7 @@
 package task_2;
 
-import task_2.exceptions.ResourceNotFoundException;
-
 public class Main {
-    public static void main(String[] args) throws ResourceNotFoundException {
+    public static void main(String[] args) {
         var runner = new TestRunner_IrakliBarikhashvili();
         runner.addScanningPackage("task_2");
         runner.addMethodAnnotation(IntensiveTest_IrakliBarikhashvili.class);
